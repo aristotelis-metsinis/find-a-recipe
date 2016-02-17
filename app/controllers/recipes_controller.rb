add801afb793e858b9e433f350d2f9b1ed9a5346
aristotelis metsinis <aristotelis.metsinis@persado.com>
2016-02-17 19:27:57 +0200

    commit first version of app

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,13 @@
+# RecipesController that has an index action.
+class RecipesController < ApplicationController
+  
+	# The RecipesController index action should:
+	#	- check if a request parameter search was passed in.
+	#	- use the search term as the keyword if supplied, and use a default value of chocolate if not supplied.
+	def index
+  
+		@search_term = params[ :search ] || 'chocolate'
+		@recipes = Recipe.for( @search_term )
+  
+  end
+end
